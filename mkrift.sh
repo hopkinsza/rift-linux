0558f0994c6b50025e0ead45aeac1c8bc2bac478
zach hopkins <hopkinsza@gmail.com>
2021-12-12 06:22:30 -0500

    update

--- mkrift.sh
+++ mkrift.sh
@@ -1,81 +1,112 @@
 #!/bin/ksh
 
-warnx() {
-	printf -- "$0: "
-	printf -- "$@\n"
-}
-errx() {
-	e=$1; shift
-	warnx "$@"
-	exit $e
-}
+####
+#### Sanity check
+####
 
-populate_common() {
-	mkdir -p bin etc include lib libexec sbin share/man src
-	ln -s share/man man
-}
+if ! [ -r err.subr ]; then
+	echo 'please run in the project directory'
+	exit 1
+fi
 
-d=rift-root
+. ./err.subr
 
-
-#
 # Run as root
-#
 
 [[ $(id -u) != 0 ]] && errx 1 "must run as root"
 
+export LC_ALL=C
+umask 022
 
-### 
-### Set up the main filesystem
-### 
 
-#
-# Directory
-#
+####
+#### Functions
+####
+
+populate () {
+	if [ $# -ne 1 ]; then
+		errx 'populate(): wrong number of args'
+	fi
+
+	# common to all
+	mkdir bin sbin
+	mkdir lib libexec
+	# libdata?
+
+	usr_and_local='games include share src'
+
+	case "$1" in
+	root)
+		mkdir etc
+		mkdir boot dev home mnt opt proc root sys tmp usr var
+
+		cd var
+		populate-var
+		cd ..
+		;;
+	usr)
+		mkdir $usr_and_local
+		mkdir local
+		;;
+	local)
+		mkdir $usr_and_local
+		mkdir etc
+		;;
+	*)
+		errx 'populate(): unrecognized arg'
+		;;
+	esac
+}
+
+populate-var () {
+	mkdir backups cron log mail run spool tmp
+	ln -s ../run ./run
+
+	# more possibilities:
+	# cache, crash, games, msgs
+	# db, preserve (bsd)
+	# lib (linux)
+
+}
+
+
+####
+#### Set up the filesystem
+####
+
+d=rift-root
 
 [[ -e $d ]] && errx 1 "$d already exists"
 
-umask 022
 mkdir $d
 cd $d
 
-mkdir bin boot dev etc home mnt opt proc root sbin srv sys tmp usr var
+populate root
+
+cd usr
+populate usr
+
+cd local
+populate local
+
+cd ../..
 
 #
 # Populate /dev
 #
 
-# the /dev/console node is used by init to make the console its ctty,
-# before /etc/rc is run and mounts devtmpfs
+# The /dev/console node is used by init to make the console its ctty,
+# before /etc/rc is run and mounts devtmpfs.
+# The shell complains if /dev/tty doesn't exist.
 cd dev
 mknod console c 5 1
 mknod tty c 5 0
 cd ..
 
 #
-# Populate /usr
+# Permissions
 #
 
-cd usr
-mkdir bin games include lib local sbin share
-# /usr/local
-cd local
-populate_common
-cd ..
-#
-cd ..
-
-#
-# Populate /var
-#
-
-mkdir backups cache cron lib lock log mail run spool tmp
-
-#
-# Symlinks/permissions
-#
-
-ln -s var/run run
 chmod 1777 tmp var/tmp
 
 
